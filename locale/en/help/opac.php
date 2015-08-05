559c819e082425a3c080ecf8d5783f9fea3a53d0
xelite25 <xelite25@users.noreply.github.com>
2015-08-05 15:55:09 -0700

    Table Inserted

--- locale/en/help/opac.php
+++ locale/en/help/opac.php
@@ -3,6 +3,31 @@
 Help Sub Sections:
 <ul>
   <li><a href="#sec1"><font class="error">This help system is still under construction.</font></a></li>
-  <li><a href="#sec2"><center>For an additional search tips press HERE</center>></font></a></li>
+  <li><a href="#sec2"><hr>
+<center>
+<h2><font color="blue">Keyword Searches</h2>
+<table border="2">
+<tbody>
+<tr>
+<th>Search Type</th>
+<th>What It Looks For</th>
+</tr>
+<tr>
+<td>Keyword</td>
+<td>Any word matching anywhere in the record</td>
+</tr>
+<tr>
+<td>Title</td>
+<td>Words matching the Titles in our database</td>
+</tr>
+<tr>
+<td>Subject</td>
+<td>Words matching the subjects in our database</td>
+</tr>
+<tr>
+<td>Call Number</td>
+<td>Any number matching the call number.</td>
+</tr>
+</center>
 </ul>
 <br><br>
