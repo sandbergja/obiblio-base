f85417cacd4d9696a33f9067a33d72f27ff9997f
Jane Sandberg <sandbergja@users.noreply.github.com>
2016-01-25 12:30:31 -0800

    Create catalogingWorkflow.php

---
+++ locale/en/help/catalogingWorkflow.php
@@ -0,0 +1,33 @@
+1. Open up Mozilla Firefox browser and go to http://esol.lbcc.linnlibraries.org/
+
+2. Log In
+
+3. Click on “Catalogue” heading
+
+4. Click on “Copy Catalogue”
+
+5. Scan/manually input ISBN (back of book)
+
+a. If record comes up, all you have to do is add Call Number (= ELA Level), and Submit
+
+b. Scan Bar Code (Done!)
+
+6. If no record comes up, click on “New Record”
+
+7. Scan/manually input ISBM
+
+a. Add book title
+
+b. Add last name of one author (in “personal name” section)
+
+c. Add ELA level (in “call number” section)
+
+8.  Submit
+
+9. Scan/manually input bar code
+
+a. If you have more than one book with the same title, you can click on “Add New Copy” 
+
+10. Done!
+
+and scan/ manually input bar code of this book (repeat as necessary)
